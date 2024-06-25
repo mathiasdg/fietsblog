d1b35d5f24f8abe454c38a6f1eb3c9110d832e68
mathiasdg <mathias.de.gucht@gmail.com>
2024-06-25 11:44:55 +0200

    vanalle updates aan layout maar bug na build voor preview

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,35 @@
+import "animate.css";
 import "../styles/index.css";
 import "leaflet-geometryutil";
 import ProgressBar from "progressbar.js";
 import MapHandler from "./mapHandler";
 
 // globals and constants
+const animationDuration = 4444;
 const extraInfo = document.getElementById("extra-info");
+const statsKnop = document.getElementById("statz");
+const statsModal = document.getElementById("statz-modal-body");
+const vuurwerk = document.getElementById("vuurwerkers");
+const logos = document.querySelector(".logo");
+
+// load the stats from the external file
+statsModal.innerHTML = await fetch("./stats.html").then((res) => res.text());
+
+statsKnop.addEventListener("click", stopAnimations);
+setTimeout(animateLogos, animationDuration + 3690);
+
+function animateLogos() {
+  const rubberOfSwing =
+    Math.random() > 0.5 ? "animate__rubberBand" : "animate__swing";
+  logos.classList.add(rubberOfSwing, "animate__infinite", "animate__slower");
+
+  setTimeout(() => {
+    vuurwerk.classList.add("animate__zoomOutDown");
+  }, animationDuration + 6900);
+}
+function stopAnimations() {
+  logos.classList.remove("animate__animated");
+}
 
 // Initialize the MapHandler
 const maphandy = new MapHandler();
@@ -12,16 +37,16 @@ const routeData = await maphandy.processData();
 // maphandy.processData(); // tweede achtervolger => Eefke
 
 const totaalKilometers = Number(routeData.lengteTotaleRoute / 1000).toFixed(0);
-let afgelegdeKilometers = (Number) ((routeData.lengteAfgelegdeRoute / 1000).toFixed(0));
+let afgelegdeKilometers = Number(
+  (routeData.lengteAfgelegdeRoute / 1000).toFixed(0)
+);
 afgelegdeKilometers = afgelegdeKilometers; // +20km voor Lier-Mechelen
-const afgelegdPercentage = (
-  afgelegdeKilometers / totaalKilometers
-).toFixed(2);
+const afgelegdPercentage = (afgelegdeKilometers / totaalKilometers).toFixed(2);
 
 // Initialize ProgressBar
 const bar = new ProgressBar.Line("#bar", {
   easing: "easeOut",
-  duration: 4690,
+  duration: animationDuration,
   color: "#116944",
   trailColor: "#ff6944",
   svgStyle: { width: "100%", height: "100%" },
@@ -35,6 +60,6 @@ const intervalID = setInterval(() => {
   bar.setText(`${(bar.value() * 100).toFixed(0)}%`);
   const afgelegdeKilis = (bar.value() * totaalKilometers).toFixed(0);
   extraInfo.innerHTML = `Reeds bestierde afstand: <b>${afgelegdeKilis}</b> van de ${totaalKilometers} kilometer <img src='/images/bull.svg'/>`;
-}, 10);
+}, 11);
 
-setTimeout(() => clearInterval(intervalID), 5000);
+setTimeout(() => clearInterval(intervalID), animationDuration + 169);
